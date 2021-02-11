a093bbcac9d4b568ac2cb37472ab67681b9e31c0
rachione <lychnussys@gmail.com>
2021-02-12 00:18:38 +0800

    fix: Rect classes

--- imgProcess.py
+++ imgProcess.py
@@ -1,6 +1,14 @@
 import cv2
 import uuid
 import numpy as np
+from enum import Enum
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r"D:\exe\Tesseract-OCR\tesseract.exe"
+
+
+class Rect(Enum):
+    WORD = 1
+    PANEL = 2
 
 
 class TrackbarDebug:
@@ -39,9 +47,65 @@ class TrackbarDebug:
         return img
 
 
+class RectModule:
+    def __init__(self):
+        self.debug = TrackbarDebug()
+
+    def isGoodRange(self, rect, type):
+        x, y, w, h = rect
+        return {
+            Rect.WORD: (h > 15 and w > 5 and w < 150 and h < 150),
+            Rect.PANEL: (h > 20 and w > 30 and w < 300 and h < 300),
+        }[type]
+
+    def sort(self, rects):
+        return sorted(rects, key=lambda rect: rect[2] * rect[3])
+
+    #remove contained and overlapped rects
+    def removeBadRects(self, rects):
+        rects = self.sort(rects)
+        for rect1 in rects[:]:
+            x1, y1, w1, h1 = rect1
+            for rect2 in rects[:]:
+                x2, y2, w2, h2 = rect2
+                if rect1 == rect2:
+                    continue
+
+                isContain=(x2 + w2) >= (x1 + w1)\
+                        and x2 <= x1\
+                        and y2 <= y1\
+                        and (y2 + h2) >= (y1 + h1)
+
+                isOverlap= not((x1>=(x2 + w2))\
+                        or ((x1+w1)<=x2)\
+                        or (y1>=(y2+h2))\
+                        or ((y1+h1)<=y2))
+                if isContain or isOverlap:
+                    rects.remove(rect1)
+                    break
+
+        return rects
+
+    def getBiggerRect(self, img, rects):
+        imgH, imgW, _ = img.shape
+        for i in range(len(rects)):
+            x, y, w, h = rects[i]
+            rects[i] = max(0, x - 3), max(0,y - 3),\
+                        min(imgW,w + 6), min(imgH, h + 6)
+
+        return rects
+
+    def getGoodRect(self, cnts, rectType):
+        rects = [cv2.boundingRect(cnt) for cnt in cnts]
+        rects = [rect for rect in rects if self.isGoodRange(rect, rectType)]
+        rects = self.removeBadRects(rects)
+        return rects
+
+
 class ImgProcess:
     def __init__(self):
         self.debug = TrackbarDebug()
+        self.rectModule = RectModule()
 
     def imwrite(self, img):
         name = str(uuid.uuid1())
@@ -74,8 +138,8 @@ class ImgProcess:
         return avgColor
 
     def getColorThreshold(self, img, pos):
-        max_dist = 60
-        avgColor = self.getAverageColor(img, pos, 30)
+        max_dist = 20
+        avgColor = self.getAverageColor(img, pos, 10)
         R = img[..., 0].astype(np.float32)
         G = img[..., 1].astype(np.float32)
         B = img[..., 2].astype(np.float32)
@@ -90,6 +154,22 @@ class ImgProcess:
         res = self.getThresholdOtsu(res)
 
         return res
+
+    def getWordMask(self, img):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        edge = cv2.Canny(gray, 280, 500)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+        dilate = cv2.dilate(edge, kernel, iterations=4)
+
+        cnts, _ = cv2.findContours(dilate, cv2.RETR_TREE,
+                                   cv2.CHAIN_APPROX_SIMPLE)
+        rects = self.rectModule.getGoodRect(cnts, Rect.WORD)
+        mask = np.zeros([img.shape[0], img.shape[1]], dtype=np.uint8)
+        for rect in rects:
+            x, y, w, h = rect
+            cv2.rectangle(mask, (x, y), (x + w, y + h), (255, 255, 255), -1)
+
+        return mask
 
     def getThresholdOtsu(self, img):
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -111,7 +191,7 @@ class ImgProcess:
             peri = cv2.arcLength(cnt, True)
             cnt = cv2.approxPolyDP(cnt, 0.02 * peri, True)
             x, y, w, h = cv2.boundingRect(cnt)
-            if h > 20 and w > 30 and w < 500 and h < 500:
+            if h > 20 and w > 30 and w < 300 and h < 300:
                 goodRects.append((x, y, w, h))
 
         return goodRects
@@ -127,23 +207,31 @@ class ImgProcess:
         return bestRect
 
     def getUIContour(self, origin, pos):
-
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+        wordMask = self.wordDetect.getWordMask(origin)
         colorThresh = self.getColorThreshold(origin, pos)
+        mergeThresh = cv2.add(wordMask, colorThresh)
+        mergeThresh_inv = cv2.bitwise_not(mergeThresh)
+
+        resThresh = cv2.dilate(mergeThresh_inv, kernel, iterations=3)
+
         # edge = self.debug.imshow(
         #     40, 150, (lambda min, max: cv2.Canny(colorThresh, min, max)))
-        edge = cv2.Canny(colorThresh, 40, 150)
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
-        dilate = cv2.dilate(edge, kernel)
-        rects = self.getRects(dilate, origin)
+        # edge = cv2.Canny(resThresh, 40, 150)
+        # dilate = cv2.dilate(edge, kernel, iterations=3)
+        rects = self.getRects(resThresh, origin)
         rect = self.getBestRect(rects, pos)
 
-        # area = np.copy(origin)
-        # for r in rects:
-        #     x, y, w, h = r
-        #     cv2.rectangle(area, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        # cv2.imshow('colorThresh', colorThresh)
-        # cv2.imshow('dilate', dilate)
-        # cv2.imshow('area', area)
-        # cv2.waitKey(0)
+        area = np.copy(origin)
+        for r in rects:
+            x, y, w, h = r
+            cv2.rectangle(area, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        cv2.imshow('wordMask', wordMask)
+        cv2.imshow('colorThresh', colorThresh)
+        cv2.imshow('mergeThresh_inv', mergeThresh_inv)
+        cv2.imshow('resThresh', resThresh)
+        cv2.imshow('area', area)
+        cv2.waitKey(0)
 
         return rect
--- testComponent/ShapeDetection.py
+++ testComponent/ShapeDetection.py
@@ -5,25 +5,33 @@ import pytesseract
 from pathlib import Path
 sys.path.insert(0, '..')
 from imgProcess import *
-pytesseract.pytesseract.tesseract_cmd = r"D:\EXE\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"D:\exe\Tesseract-OCR\tesseract.exe"
 
 
-def isGoodRect(rect):
+def isGoodRange(rect):
     x, y, w, h = rect
     return (h > 15 and w > 5 and w < 150 and h < 150)
 
 
-def removeContains(rects):
+#remove contained and overlapped rects
+def removeBadRects(rects):
     for rect1 in rects[:]:
         x1, y1, w1, h1 = rect1
         for rect2 in rects[:]:
             x2, y2, w2, h2 = rect2
             if rect1 == rect2:
                 continue
-            if (x2 + w2) >= (x1 + w1)\
+
+            isContain=(x2 + w2) >= (x1 + w1)\
                     and x2 <= x1\
                     and y2 <= y1\
-                    and (y2 + h2) >= (y1 + h1):
+                    and (y2 + h2) >= (y1 + h1)
+
+            isOverlap= not((x1>=(x2 + w2))\
+                    or ((x1+w1)<=x2)\
+                    or (y1>=(y2+h2))\
+                    or ((y1+h1)<=y2))
+            if isContain or isOverlap:
                 rects.remove(rect1)
                 break
 
@@ -34,19 +42,17 @@ def getBiggerRect(img, rects):
     imgH, imgW, _ = img.shape
     for i in range(len(rects)):
         x, y, w, h = rects[i]
-        rects[i] = max(0, x - 10), max(0, y - 10), min(imgW,
-                                                       w + 20), min(imgH, h + 20)
+        rects[i] = max(0, x - 3), max(0, y - 3), min(imgW,
+                                                     w + 6), min(imgH, h + 6)
 
     return rects
 
 
 debug = TrackbarDebug()
 img = cv2.imread('testImg/s1.jpg')
-
+area = np.copy(img)
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# edge = debug.imshow(
-#     280, 500, (lambda min, max: cv2.Canny(gray, min, max)))
 edge = cv2.Canny(gray, 280, 500)
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
 dilate = cv2.dilate(edge, kernel, iterations=4)
@@ -54,22 +60,23 @@ dilate = cv2.dilate(edge, kernel, iterations=4)
 cnts, _ = cv2.findContours(dilate, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 cnts = sorted(cnts, key=lambda x: cv2.contourArea(x))
 rects = [cv2.boundingRect(cnt) for cnt in cnts]
-rects = [rect for rect in rects if isGoodRect(rect)]
-rects = removeContains(rects)
+rects = [rect for rect in rects if isGoodRange(rect)]
+rects = removeBadRects(rects)
+rects = getBiggerRect(img, rects)
 
 # for rect in rects:
 #     x, y, w, h = rect
 #     cv2.rectangle(area, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-area = np.copy(img)
 OTSUtest = np.zeros([img.shape[0], img.shape[1]], dtype=np.uint8)
 for rect in rects:
     x, y, w, h = rect
     ROI = gray[y:y + h, x:x + w]
     _, ROI = cv2.threshold(ROI, 128, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     OTSUtest[y:y + h, x:x + w] = ROI
-    word = pytesseract.image_to_string(
-        ROI, lang='jpn', config='--psm 10 --oem 1')
+    word = pytesseract.image_to_string(ROI,
+                                       lang='jpn',
+                                       config='--psm 10 --oem 1')
     word = word.replace(' ', '').replace('\f', '').replace('\n', '')
     if word != '':
         print(word)
