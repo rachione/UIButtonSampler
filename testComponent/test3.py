b7a93452998e58fea3b71215aacf81978ccde225
rachione <lychnussys@gmail.com>
2021-03-01 23:52:54 +0800

    feat: new testing

--- testComponent/test3.py
+++ testComponent/test3.py
@@ -9,16 +9,21 @@ def angle_cos(p0, p1, p2):
 
 def find_squares_entity(bin):
     squares = []
-    contours, _hierarchy = cv.findContours(
-        bin, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+    contours, _hierarchy = cv.findContours(bin, cv.RETR_LIST,
+                                           cv.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
         x, y, w, h = cv.boundingRect(cnt)
         cnt_len = cv.arcLength(cnt, True)
         cnt = cv.approxPolyDP(cnt, 0.02 * cnt_len, True)
-        if len(cnt) == 4 and h > 20 and w > 30 and w < 300 and h < 300 and cv.isContourConvex(cnt):
+        if len(
+                cnt
+        ) == 4 and h > 20 and w > 30 and w < 300 and h < 300 and cv.isContourConvex(
+                cnt):
             cnt = cnt.reshape(-1, 2)
-            max_cos = np.max(
-                [angle_cos(cnt[i], cnt[(i + 1) % 4], cnt[(i + 2) % 4]) for i in range(4)])
+            max_cos = np.max([
+                angle_cos(cnt[i], cnt[(i + 1) % 4], cnt[(i + 2) % 4])
+                for i in range(4)
+            ])
             if max_cos < 0.3:
                 squares.append(cnt)
 
@@ -41,7 +46,7 @@ def find_squares(img):
 
 def main():
 
-    img = cv.imread('testImg/s1.jpg')
+    img = cv.imread('testImg/s5.jpg')
     squares = find_squares(img)
     cv.drawContours(img, squares, -1, (0, 255, 0), 3)
     cv.imshow('squares', img)
---
+++ testComponent/test4.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+cnt = np.array([[[531, 422]], [[534, 494]], [[780, 492]], [[778, 420]]])
+print(cnt)
+cnt = cnt.reshape((-1, 2))
+print(cnt)
